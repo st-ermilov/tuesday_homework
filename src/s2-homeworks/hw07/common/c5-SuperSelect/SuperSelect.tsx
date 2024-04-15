fdb4d36db951e8fc5223844a3186ddc3d9b6faae
Stanislav Ermilov <shindji19922904@gmail.com>
2024-04-15 14:00:14 +0700

    update HW12

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -37,7 +37,7 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         // делают студенты
-        if(onChangeOption) onChangeOption(e.currentTarget.value)
+        if(onChangeOption) onChangeOption(+e.currentTarget.value)
         // console.log('onChangeCallback' + e.currentTarget.value)
 
     }
