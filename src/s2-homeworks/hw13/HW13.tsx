ba0d6ef4b1c9bdb51d709ccb7adbff44cc498705
Stanislav Ermilov <shindji19922904@gmail.com>
2024-04-25 09:19:36 +0700

    HW13 update errors messages

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -36,8 +36,7 @@ const HW13 = () => {
             .then((res) => {
                 setCode('Код 200!')
                 setImage(success200)
-                setText('...всё ок)\n' +
-                    'код 200 - обычно означает что скорее всего всё ок)')
+                setText('...всё ок)')
                 setInfo('')
 
             })
@@ -48,22 +47,19 @@ const HW13 = () => {
                         setInfo('')
                         setCode('Ошибка 500!')
                         setImage(error500)
-                        setText('эмитация ошибки на сервере\n' +
-                            'ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
+                        setText('эмитация ошибки на сервере')
                         break
                     }
                     case undefined: {
-                        console.log('Это undefined' + ' ' + e)
                         setInfo('')
                         setCode('Ошибка 400!')
                         setImage(error400)
-                        setText('Ты не отправил success в body вообще!\n' +
-                            'ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
+                        setText('Ты не отправил success в body вообще!')
                         break
                     }
                     case null: {
                         console.log('Это null' + ' ' + e)
-                        setInfo('')
+                        setInfo('Error')
                         setCode('Error!')
                         setImage(errorUnknown)
                         setText('Network Error\n' +
